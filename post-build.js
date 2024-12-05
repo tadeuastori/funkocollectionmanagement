24a1b299f0945d3cb807e54a50202ec6fae93571
Carlos Astori <tadeu.astori@gmail.com>
2024-12-05 07:05:12 -0500

    test

--- post-build.js
+++ post-build.js
@@ -1,4 +1,4 @@
 const fs = require('fs-extra');
-fs.move('docs/browser', 'dist', (err) => { if(err) { return console.error(err); } });
+fs.move('docs/browser', 'docs', (err) => { if(err) { return console.error(err); } });
 fs.remove('docs/browser', (err) => { if(err) { return console.error(err); } });
 fs.remove('dist/*', (err) => { if(err) { return console.error(err); } });
